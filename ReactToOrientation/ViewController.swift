20573c43a3d98db57e088ba09ee859cc021e83e9
Wilfried Josset <w.josset@gmail.com>
2020-03-06 15:33:36 +0100

    Refactor playground viewcontroller

--- ReactToOrientation/ViewController.swift
+++ ReactToOrientation/ViewController.swift
@@ -1,32 +1,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+	
+	
+	// MARK: - Internal
+	
+	// MARK: IBOutlets
+	
 	@IBOutlet var orientationLabel: UILabel!
 	@IBOutlet var colorView: UIView!
 	
-	// Will trigger viewDidLayoutSubviews()
+	// MARK: IBActions
+	
 	@IBAction func didTapOnChangeColorButton() {
-		colorView.backgroundColor = colors[colorIndex % colors.count]
-		colorView.frame = .zero
-		colorIndex += 1
+		changeColorView()
 	}
-
-	private let colors: [UIColor] = [.brown, .cyan, .red, .blue]
 	
-	private var colorIndex = 0
 	
-	// Change that value to update label at the indicated lifecycle steps
-	private let lifecycleStepsUpdatingLabel: [LifeCycleOrientationStep] = [
-			.viewDidAppear,
-			.traitCollectionDidChange
-		]
 	
-	private var orientation: UIDeviceOrientation { UIDevice.current.orientation }
+	
+	// MARK: - Override
+	
+	// MARK: Lifecycle - Standard
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		changeTextOfOrientationLabelIfIndicatedStep(step: .viewDidLoad)
 	}
+	
+	override func viewDidLayoutSubviews() {
+		super.viewDidLayoutSubviews()
+		changeTextOfOrientationLabelIfIndicatedStep(step: .viewDidLayoutSubviews)
+	}
+	
+	override func viewDidAppear(_ animated: Bool) {
+		super.viewDidAppear(animated)
+		changeTextOfOrientationLabelIfIndicatedStep(step: .viewDidAppear)
+	}
+	
+	
+	// MARK: Lifecycle - Related to rotation
 	
 	override func willTransition(to newCollection: UITraitCollection, with coordinator: UIViewControllerTransitionCoordinator) {
 		super.willTransition(to: newCollection, with: coordinator)
@@ -34,9 +47,9 @@ class ViewController: UIViewController {
 		print()
 		print("❗️🥳 User has rotated the device!!!")
 		print()
+		
 		changeTextOfOrientationLabelIfIndicatedStep(step: .willTransitionNewCollection)
 	}
-	
 	
 	override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
 		super.viewWillTransition(to: size, with: coordinator)
@@ -48,48 +61,82 @@ class ViewController: UIViewController {
 		changeTextOfOrientationLabelIfIndicatedStep(step: .traitCollectionDidChange)
 	}
 	
-	override func viewDidLayoutSubviews() {
-		super.viewDidLayoutSubviews()
-		changeTextOfOrientationLabelIfIndicatedStep(step: .viewDidLayoutSubviews)
-	}
-
-	override func viewDidAppear(_ animated: Bool) {
-		super.viewDidAppear(animated)
-		changeTextOfOrientationLabelIfIndicatedStep(step: .viewDidAppear)
+	
+	
+	
+	// MARK: - Private
+	
+	
+	// MARK: Properties - Private
+	
+	// ⚠️Change lifecycleStepsUpdatingLabel to update label at the indicated lifecycle steps
+	
+	/// Defines all the lifecycle steps that should update the orientation description label
+	private let lifecycleStepsUpdatingLabel: [LifeCycleOrientationStep] = [
+			.viewDidAppear,
+			.traitCollectionDidChange
+		]
+	
+	/// Colors to indicate to the user that the button has been pushed
+	private let colors: [UIColor] = [.brown, .cyan, .red, .blue]
+	private var colorIndex = 0
+	
+	/// Interface orientation
+	private var windowInterfaceOrientation: UIInterfaceOrientation? {
+		return UIApplication.shared.windows.first?.windowScene?.interfaceOrientation
 	}
 	
-	private func changeTextOfOrientationLabelIfIndicatedStep(step: LifeCycleOrientationStep) {
-		guard let interfaceOrientation = windowInterfaceOrientation else { return }
+	/// Physical device orientation
+	private var deviceOrientation: UIDeviceOrientation { UIDevice.current.orientation }
+	
+	/// Description which describes the current device orientation together with the interface orientation
+	private var orientationDescription: String {
+		guard let interfaceOrientation = windowInterfaceOrientation else { return "" }
 		
-		print()
-		print("\(step.colorIndicator) \(step)")
-		
-		
-		let orientationDescription =
+		let description =
 		"""
 		📱Device Orientation:
-		Is in portrait mode => \(orientation.isPortrait ? "✅" : "❌")
-		Is in landscape mode => \(orientation.isLandscape ? "✅" : "❌")
-		Is in flat mode => \(orientation.isFlat ? "✅" : "❌")
-		Is in valid orientation => \(orientation.isValidInterfaceOrientation ? "✅" : "❌")
+		Is in portrait mode => \(deviceOrientation.isPortrait ? "✅" : "❌")
+		Is in landscape mode => \(deviceOrientation.isLandscape ? "✅" : "❌")
+		Is in flat mode => \(deviceOrientation.isFlat ? "✅" : "❌")
+		Is in valid orientation => \(deviceOrientation.isValidInterfaceOrientation ? "✅" : "❌")
 		
 		⬆️Interface Orientation:
 		Is in portrait mode => \(interfaceOrientation.isPortrait ? "✅" : "❌")
 		Is in landscape mode => \(interfaceOrientation.isLandscape ? "✅" : "❌")
 		"""
 		
-		print(orientationDescription)
-		print()
+		return description
+	}
+	
+	
+	
+	// MARK: Methods - Private
+	
+	private func changeTextOfOrientationLabelIfIndicatedStep(step: LifeCycleOrientationStep) {
+		printOrientationDescriptionToConsole(step: step)
 		
 		if lifecycleStepsUpdatingLabel.contains(step) {
 			orientationLabel.text = orientationDescription
 		}
 	}
 	
-	private var windowInterfaceOrientation: UIInterfaceOrientation? {
-		return UIApplication.shared.windows.first?.windowScene?.interfaceOrientation
+	private func printOrientationDescriptionToConsole(step: LifeCycleOrientationStep) {
+		print()
+		print(step.description)
+		print(orientationDescription)
+		print()
+	}
+	
+	/// Function which change the color view bound in order to trigger viewDidLayoutSubviews()
+	private func changeColorView() {
+		colorView.backgroundColor = colors[colorIndex % colors.count]
+		colorView.frame = .zero
+		colorIndex += 1
 	}
 }
+
+
 
 
 enum LifeCycleOrientationStep: String {
@@ -101,7 +148,11 @@ enum LifeCycleOrientationStep: String {
 	viewDidLayoutSubviews,
 	viewDidAppear
 	
-	var colorIndicator: String {
+	var description: String {
+		return "\(self.colorIndicator) \(self)"
+	}
+	
+	private var colorIndicator: String {
 		switch self {
 		case .viewDidLoad: return "🔵"
 		case .willTransitionNewCollection: return "🟣"
